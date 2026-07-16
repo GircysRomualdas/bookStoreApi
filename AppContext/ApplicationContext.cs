87c3c7e740255c20c09c0d20fb43cf1939bcc60e
Romas <r2udis@gmail.com>
2026-07-17 02:30:25 +0300

    Add app context

---
+++ AppContext/ApplicationContext.cs
@@ -0,0 +1,16 @@
+
+using BookStoreApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApi.AppContext;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options) {
+  private const string DefaultSchema = "bookapi";
+  public DbSet<BookModel> Books { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder) {
+    base.OnModelCreating(modelBuilder);
+    modelBuilder.HasDefaultSchema(DefaultSchema);
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+  }
+}
