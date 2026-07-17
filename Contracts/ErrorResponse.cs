49f4569ec051b5eb2241a527bed9ffae99ce6de8
Romas <r2udis@gmail.com>
2026-07-18 00:40:09 +0300

    Add DTO contracts

---
+++ Contracts/ErrorResponse.cs
@@ -0,0 +1,7 @@
+namespace BookStoreApi.Contracts;
+
+record ErrorResponse {
+  public string Title { get; set; } = "";
+  public int StatusCode { get; set; }
+  public string Message { get; set; } = "";
+}
